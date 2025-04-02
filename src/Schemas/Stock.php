1e5532953d15d1b663d2fbb7a1104ce601eb8f75
hamzah <hamzahnuralfalah@gmail.com>
2025-04-02 20:37:30 +0700

    update

--- src/Schemas/Stock.php
+++ src/Schemas/Stock.php
@@ -2,7 +2,6 @@
 
 namespace Hanafalah\ModuleWarehouse\Schemas;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Hanafalah\LaravelSupport\Supports\PackageManagement;

756c2874b0f98dcd66d569bccbc3839bf1371494
hamzah <hamzahnuralfalah@gmail.com>
2025-04-02 20:44:30 +0700

    modules baru

--- src/Contracts/ModuleWarehouse.php
+++ src/Contracts/ModuleWarehouse.php
@@ -2,6 +2,6 @@
 
 namespace Hanafalah\ModuleWarehouse\Contracts;
 
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface ModuleWarehouse extends DataManagement {}

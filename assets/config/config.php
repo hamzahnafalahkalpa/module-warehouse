0bcc031b274ac4045ca0d458d8b3f74af39c9eab
Hamzah <hamzahnafalah@gmail.com>
2025-05-03 12:50:30 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -6,33 +6,23 @@ use Hanafalah\ModuleWarehouse\{
 };
 
 return [
-    'contracts' => [
-        'building'           => Contracts\Building::class,
-        'room'               => Contracts\Room::class,
-        'stock'              => Contracts\Stock::class,
-        'batch'              => Contracts\Batch::class,
-        'stock_movement'     => Contracts\StockMovement::class,
-        'batch_movement'     => Contracts\BatchMovement::class,
-        'model_has_room'     => Contracts\ModelHasRoom::class,
-        'stock_batch'        => Contracts\StockBatch::class,
-        'goods_receipt_unit' => Contracts\GoodsReceiptUnit::class
+    'namespace' => 'Hanafalah\\ModuleWarehouse',
+    'app' => [
+        'contracts' => [
+            //ADD YOUR CONTRACTS HERE
+        ],
+    ],
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts',
+        'schema' => 'Schemas',
+        'database' => 'Database',
+        'data' => 'Data',
+        'resource' => 'Resources',
+        'migration' => '../assets/database/migrations'
     ],
     'database' => [
         'models' => [
-            'Building'         => ModuleWarehouseModels\Building\Building::class,
-            'Room'             => ModuleWarehouseModels\Building\Room::class,
-            'Stock'            => ModuleWarehouseModels\Stock\Stock::class,
-            'MainStock'        => ModuleWarehouseModels\Stock\MainStock::class,
-            'MainMovement'     => ModuleWarehouseModels\Stock\MainMovement::class,
-            'Batch'            => ModuleWarehouseModels\Stock\Batch::class,
-            'StockMovement'    => ModuleWarehouseModels\Stock\StockMovement::class,
-            'BatchMovement'    => ModuleWarehouseModels\Stock\BatchMovement::class,
-            'Storage'          => ModuleWarehouseModels\Storage\Storage::class,
-            'ModelHasStorage'  => ModuleWarehouseModels\Storage\ModelHasStorage::class,
-            'ModelHasRoom'     => ModuleWarehouseModels\ModelHasRoom\ModelHasRoom::class,
-            'StockFunding'     => ModuleWarehouseModels\Stock\StockFunding::class,
-            'StockBatch'       => ModuleWarehouseModels\Stock\StockBatch::class,
-            'GoodsReceiptUnit' => ModuleWarehouseModels\Stock\GoodsReceiptUnit::class
         ]
     ],
     'warehouse' => ModuleWarehouseModels\Building\Room::class
